cbf0a1e2a88181b2ca56e2f8b7ece5f3ce84f86d
BrandonAmyot <amyot.brandon@hotmail.com>
2016-11-08 22:39:59 -0500

    Map Class
    up to date version

---
+++ Driver.cpp
@@ -0,0 +1,48 @@
+//! @file
+//! @brief driver file for implementation
+//!
+#include <iostream>
+#include "Map.h"
+using namespace std;
+
+int main()
+{/*
+	Map* adventure = new Map(10,15,6,7,0,2,"more");
+	adventure->printMap();
+	adventure->save("game");
+
+	Map* m = new Map(10,15,0,0,0,0,"yo");
+	m->load("more");
+	m->printMap();
+	cout << "\n\n\n" << m->getStartX() << "and" << m->getStartY() << "\n";
+	*/
+	
+	//Map::load("mortimer2")->printMap();
+
+	/*Map::createMap();
+	Map* map = new Map();
+	map->load("mortimer.txt");
+	cout << "\n\nTHIS IS THE LOADED ONE\n\n";
+	map->printMap();*/
+	//Map::editMap("maptest");
+	
+
+	
+	//Map::createMap();
+	
+	//Map::editMap("wub.txt");
+
+	//Map* map = new Map();
+	//map->load("adv.txt");
+	//map->printMap();
+	//cout<<"\n\nSTART IS:"<<map->getStartX()<<" and "<<map->getStartY()<<" and end coord is: "<<map->getEndX()<< " and "<<map->getEndY()<<"\n\n\n" ;
+	//cout << "\n\nIf occupied:\n\n";
+	//cout<<map->isOccupied(0, 3)<<endl;
+	//cout << map->isOccupied(0, 0) << endl;
+	//cout << map->isOccupied(3, 4) << endl;
+	
+
+
+	return 0;
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Map
+The map header file, cpp file, and test driver.
+
+## todo
+1. map builder.
+2. link character/ item objects to the map.
